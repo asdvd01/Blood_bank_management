e4af752ae6f649f25b809f5d97d8a4a8d4db4688
Sudhanshu patel <sudhanshuptl13@gmail.com>
2014-04-15 19:22:23 +0530

    adding description and keyword
    adding description and keyword

--- php_files/index.php
+++ php_files/index.php
@@ -5,8 +5,8 @@
 
 <head>
   <title>Blood_Bank</title>
-  <meta name="description" content="website description" />
-  <meta name="keywords" content="website keywords, website keywords" />
+  <meta name="description" content="blood bank management system NIT Rourkela" />
+  <meta name="keywords" content="blood bank,donate blood in rourkela,blood information,hospital detail rourkela" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="css/style_index.css" />
   <link rel="icon"  type="image/png"   href="favicon.png" />
